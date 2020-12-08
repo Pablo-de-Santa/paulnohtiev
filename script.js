8b939e2246baa3157ebcaea99e9519b6e0c1bf71
Paul Nohtiev <nogtevpasha97@gmail.com>
2020-12-08 09:30:01 -0500

    changes in planets

--- script.js
+++ script.js
@@ -1,5 +1,171 @@
-gsap.registerPlugin(ScrollTrigger);
- 
+"use strict";
+
+gsap.registerPlugin(ScrollTrigger); 
+
+gsap.timeline({
+    scrollTrigger:{
+        trigger:"#top",
+        start:"center center",
+        end:"bottom -300%",
+        markers: false,
+        scrub : true,
+        pin : true
+    }
+})
+
+.from("#glow", {
+    scaleY : 1,
+    scaleX : 1
+})
+.to("#glow", {
+    scaleY : 0,
+    scaleX : 0
+})
+
+.from("#anotherGlow", {
+    scaleY : 1,
+    scaleX : 1
+})
+.to("#anotherGlow", {
+    scaleY : 0,
+    scaleX : 0
+})
+
+.from("#bigBang", {
+    scaleY : 0,
+    scaleX : 0,
+    opacity : 1
+})
+.to("#bigBang", {
+    scaleY : 1.5,
+    scaleX : 1.5,
+    opacity : 1,
+});
+
+gsap.timeline({
+    scrollTrigger:{
+        trigger:"#afterBang",
+        start:"center center",
+        end:"bottom -1000%",
+        markers: false,
+        scrub : true,
+        pin : true,
+    }
+})
+
+.from("#afterBang", {opacity : 0})
+.from("#space", {
+    opacity : 0, 
+})
+
+.from("#imgOfTheSun", {
+    opacity : 0, 
+    scaleY : 14,
+    scaleX : 14,
+})
+.to("#imgOfTheSun", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheMercury", {
+    opacity : 0, 
+    scaleY : 54,
+    scaleX : 54,
+    x : innerWidth * 1,
+    y : innerHeight * 1
+})
+.to("#imgOfTheSun", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheVenus", {
+    opacity : 0, 
+    scaleY : 24,
+    scaleX : 24,
+    x : innerWidth * -1,
+    y : innerHeight * -1
+})
+.to("#imgOfTheVenus", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheEarth", {
+    opacity : 0, 
+    scaleY : 20,
+    scaleX : 20,
+    x : innerWidth * 1,
+    y : innerHeight * -1
+})
+.to("#imgOfTheEarth", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheMars", {
+    opacity : 0, 
+    scaleY : 50,
+    scaleX : 50,
+    x : innerWidth * -1,
+    y : innerHeight * 1
+})
+.to("#imgOfTheMars", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheJupiter", {
+    opacity : 0, 
+    scaleY : 13,
+    scaleX : 13,
+    x : innerWidth * 0.3,
+    y : innerHeight * -0.5
+})
+.to("#imgOfTheJupiter", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheSaturn", {
+    opacity : 0, 
+    scaleY : 17,
+    scaleX : 17,
+    x : innerWidth * -0.8,
+    y : innerHeight * 0.1
+})
+.to("#imgOfTheSaturn", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheUranus", {
+    opacity : 0, 
+    scaleY : 27,
+    scaleX : 27,
+    x : innerWidth * 0.5,
+    y : innerHeight * -1
+})
+.to("#imgOfTheUranus", {
+    scaleY : 1,
+    scaleX : 1
+})
+
+.from("#imgOfTheNeptune", {
+    opacity : 0, 
+    scaleY : 30,
+    scaleX : 30,
+    x : innerWidth * -1,
+    y : innerHeight * 0.5
+})
+.to("#imgOfTheNeptune", {
+    scaleY : 1,
+    scaleX : 1
+});
+
+
+
 gsap.timeline({
      scrollTrigger:{
          trigger:".box",
@@ -15,8 +181,8 @@ gsap.timeline({
  .from(".text2", {x : innerWidth * -1})
  .from(".text3", {x : innerWidth * 1})
  .from(".logo", {
-     y : innerHeight * 1,
-     rotate : 360
+     scaleY : 0,
+     scaleX : 0
  })
 
  gsap.timeline({
